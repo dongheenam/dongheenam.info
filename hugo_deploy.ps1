aed1704de522babc2582ceaaf76674a787d874ae
Donghee Nam <donghee.nam04@gmail.com>
2022-06-30 22:17:25 +1000

    Rename requirements.txt so that Netlify does not pick up

--- hugo_deploy.ps1
+++ hugo_deploy.ps1
@@ -1,25 +1,6 @@
-# build the site in public/
-Write-Output "building Hugo..."
-hugo --minify
-
-# deploy the website to the public s3 bucket
-Write-Output "uploading to dnam.info..."
-hugo deploy --confirm --maxDeletes -1
-
-# upload the entire project to the private s3 bucket
-Write-Output "uploading to dnam.info.public..."
-aws s3 sync . s3://dongheenam.info.source --exclude ".*" --exclude "public/*" --exclude "resources/*" --delete
-
-# upload the source codes to GitHub
-Write-Output "uploading to GitHub..."
-
-$today = (Get-Date -DisplayHint Date)
-git add *
-git commit -m "automatic update on $today"
-git push
-
-Write-Output "All complete!"
-
-
-# other useful commands
+# local server
 # hugo server --bind=192.168.1.2 --baseURL=http://192.168.1.2:1313 -D --disableFastRender --enableGitInfo
+
+# cloudinary
+venv\Scripts\Activate.ps1
+cld sync .\assets\ / --push
