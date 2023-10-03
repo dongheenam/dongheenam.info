39e82a1cd3f709666378d73b1fcaa093e2c469de
Donghee Nam <57467269+dongheenam@users.noreply.github.com>
2023-10-03 20:56:52 +1100

    Update Netlify config to install dart-sass

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# https://gohugo.io/hugo-pipes/transpile-sass-to-css/#netlify
+
+curl -LJO https://github.com/sass/dart-sass/releases/download/${DART_SASS_VERSION}/dart-sass-${DART_SASS_VERSION}-linux-x64.tar.gz
+
+tar -xf dart-sass-${DART_SASS_VERSION}-linux-x64.tar.gz
+
+rm dart-sass-${DART_SASS_VERSION}-linux-x64.tar.gz
+
+export PATH=/opt/build/repo/dart-sass:$PATH
+
+hugo --gc --minify --templateMetrics
